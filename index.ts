a88bd17c913ab89cd200ad08282e8d3cfdd64c8a
SMK <idsiemu@gmail.com>
2021-04-18 10:36:31 +0900

    소켓 세팅 완료

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import { Server } from "socket.io";
 import express from 'express'
 import http from 'http'
+import redis from 'redis'
+import secret from './secret'
+import { promisify } from 'util'
 
 const app = express();
 
@@ -11,16 +14,44 @@ const server = http.createServer(app)
 server.listen(PORT, () => console.log(PORT))
 
 // 3.0 이상부터 디폴트 사용안됨
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:1234',
+  }
+});
+
+const client = redis.createClient()
+
+const redisGetAsync = promisify(client.get).bind(client)
 
 io.on('connect', (socket) => {
-  socket.on('room', (key: string) => {
+  socket.on('room', async(key: string) => {
     socket.join(key)
+
+    const messageData = await redisGetAsync(key)
+
+    socket.emit('history', messageData)
   })
   socket.on('message', (message: Message) => {
+    saveMessage(message)
     socket.nsp.to(message.key).emit("message", message)
   })
 })
+
+const saveMessage = async(message: Message) => {
+  const { key } = message;
+
+  const data = await redisGetAsync(key)
+
+  if(!data){
+    return client.set(key, "[]")
+  }
+
+  const json = JSON.parse(data)
+  json.push(message);
+
+  client.set(key, JSON.stringify(json))
+}
 
 type Message = {
   text: string;
